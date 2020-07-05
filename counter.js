e3754a29b068f9d76ae7eb569ccac3edf74cd407
KID421 <suchild7@gmail.com>
2020-07-05 11:13:58 +0800

    完成版計數器

--- counter.js
+++ counter.js
@@ -9,28 +9,15 @@ $(document).ready(function () {
             // var 變數 = 資料
             var offset = $(this).offset();       // 取得元素位移資料
             var top = offset.top;                   // 上方
-            var left = offset.left;                 // 左方
-
-            // console.log("元素的上方：" + top);
 
             var windowTop = $(window).scrollTop();
-
-            // console.log("視窗的上方：" + windowTop);
 
             // 取得參數：數字、時間與位移
             var number = $(this).attr("data-counter-number");
             var time = $(this).attr("data-counter-time");
             var offset = $(this).attr("data-counter-offset");
 
-            //console.log("數字：" + number);
-            //console.log("時間：" + time);
-            //console.log("位移：" + offset);
-
-            // 判斷式
-            // 如果 發生什麼事 就...
-            // if (條件) { 程式 }
             if (windowTop > top - offset) {
-                //console.log("開始執行動畫...");
 
                 // 選取.動畫({參數}，{選項})
                 $(this).animate({
